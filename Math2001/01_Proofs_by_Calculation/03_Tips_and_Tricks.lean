e61d54c1cfd20934397a83195efb946b30d9dc50
csvaughen <csvaughen@gmail.com>
2024-12-31 14:02:24 -0500

    done with tips and tricks!

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -87,15 +87,31 @@ example {u v : ℚ} (h1 : u + 2 * v = 4) (h2 : u - 2 * v = 6) : u = 5 :=
 
 -- Example 1.3.8
 example {x y : ℝ} (h1 : x + y = 4) (h2 : 5 * x - 3 * y = 4) : x = 2 :=
-  sorry
+  calc
+  x = (1/8)*(3*x + 3*y + 5*x - 3*y) := by ring
+  _ = (1/8)*(3*(x + y)+ (5*x - 3*y)) := by ring
+  _ = (1/8)*(3*4 + 4) := by rw  [h1,h2]
+  _ = (1/8)*(16) := by ring
+  _ = 2 := by ring
 
 -- Example 1.3.9
 example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 :=
-  sorry
+  calc
+  a^2 - a + 3 = (a-3)*(a+2)+ 9 := by ring
+  _= (2*b)*(a + 2) + 9 := by rw [h1]
+  _= (2*b)*(a - 3 + 2 + 3 ) + 9 := by ring
+  _= (2*b)*((a-3)+ 5) + 9 := by ring
+  _= (2*b)*((2*b) + 5) + 9 := by rw [h1]
+  _= (4*b^2 + 10*b +9) := by ring
+
 
 -- Example 1.3.10
 example {z : ℝ} (h1 : z ^ 2 - 2 = 0) : z ^ 4 - z ^ 3 - z ^ 2 + 2 * z + 1 = 3 :=
-  sorry
+  calc
+  z^4 - z^3 - z^2 + 2*z + 1 = (z^2 - z + 1)*(z^2-2) + 3 := by ring
+  _ = (z^2 - z + 1)*0 + 3 := by rw [h1]
+  _ = 3 := by ring
+
 
 /-! # Exercises
 
