ed1db79265348bd495f6c8d4d6a52e5540b4c97a
csvaughen <csvaughen@gmail.com>
2025-02-12 16:21:54 -0500

    got another one done

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -177,11 +177,15 @@ example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 := by
     _ = (u + 1)*(u + 1 + 1) -1 := by ring
     _ = (v)*(v+1) - 1 := by rw[h1]
     _ = v^2 + v - 1 := by ring
-    
+
 
 example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 :=
-  sorry
+  calc
+    t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = (t^2-4)*(t^2+3*t+1) + 10*t+2 := by ring
+     _ = (0)*(t^2+3*t+1) + 10*t+2 := by rw[ht]
+     _ =10*t+2 := by ring
+     
 
 example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 :=
   sorry
