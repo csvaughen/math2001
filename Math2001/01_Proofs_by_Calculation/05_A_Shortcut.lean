305e1d7f6dc090a90f904c89162c1188014fb9ba
csvaughen <csvaughen@gmail.com>
2025-03-29 21:04:26 -0400

    done with ch 1

--- Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
+++ Math2001/01_Proofs_by_Calculation/05_A_Shortcut.lean
@@ -33,6 +33,6 @@ example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 :=
 calc
   w = (1/3)*(3*w + 1) - (1/3) := by ring
   _ = (1/3)*4 - 1/3 := by rw[h1]
+  
   _ = 4/3 - 1/3 := by ring
   _ = 1 := by ring
-
