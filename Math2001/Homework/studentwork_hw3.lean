440b569f684696695ddbf04711e0149403433eb7
csvaughen <csvaughen@gmail.com>
2026-03-09 21:19:30 -0400

    added student work hw3

---
+++ Math2001/Homework/studentwork_hw3.lean
@@ -0,0 +1,182 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+
+math2001_init
+
+/-! # Section 1.4: Proving inequalities -/
+
+-- Example 1.4.1
+example {x y : ℤ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 :=
+  calc
+    y = y + 2 * x - 2 * x := by ring
+    _ ≥ 3 - 2 * x := by rel [hy]
+    _ = 9 - 2 * (x + 3) := by ring
+    _ ≥ 9 - 2 * 2 := by rel [hx]
+    _ > 3 := by numbers
+
+
+-- Example 1.4.2
+-- Exercise: replace the words "sorry" with the correct Lean justification.
+example {r s : ℚ} (h1 : s + 3 ≥ r) (h2 : s + r ≤ 3) : r ≤ 3 :=
+  calc
+    r = (s + r + r - s) / 2 := by ring
+    _ ≤ (3 + (s + 3) - s) / 2 := by rel [h1, h2]
+    _ = 3 := by ring
+
+
+-- Example 1.4.3
+-- Exercise: type out the whole proof printed in the text as a Lean proof.
+example {x y : ℝ} (h1 : y ≤ x + 5) (h2 : x ≤ -2) : x + y < 2 :=
+  calc
+    x + y
+      ≤ x + (x + 5) := by rel [h1]
+    _ = 2 * x + 5 := by ring
+    _ ≤ 2 * -2 + 5 := by rel [h2]
+    _ < 2 := by numbers
+
+
+-- Example 1.4.4
+-- Exercise: replace the words "sorry" with the correct Lean justification.
+example {u v x y A B : ℝ} (h1 : 0 < A) (h2 : A ≤ 1) (h3 : 1 ≤ B) (h4 : x ≤ B)
+    (h5 : y ≤ B) (h6 : 0 ≤ u) (h7 : 0 ≤ v) (h8 : u < A) (h9 : v < A) :
+    u * y + v * x + u * v < 3 * A * B :=
+  calc
+    u * y + v * x + u * v
+      ≤ u * B + v * B + u * v := by rel [h4, h5]
+    _ ≤ A * B + A * B + A * v := by rel [h8, h9]
+    _ ≤ A * B + A * B + 1 * v := by rel [h2]
+    _ ≤ A * B + A * B + B * v := by rel [h3]
+    _ < A * B + A * B + B * A := by rel [h9]
+    _ = 3 * A * B := by ring
+
+-- Example 1.4.5
+-- Exercise: replace the words "sorry" with the correct Lean justification.
+example {t : ℚ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 :=
+  calc
+    t ^ 2 - 3 * t - 17
+      = t * t - 3 * t - 17 := by ring
+    _ ≥ 10 * t - 3 * t - 17 := by rel [ht]
+    _ = 7 * t - 17 := by ring
+    _ ≥ 7 * 10 - 17 := by rel [ht]
+    _ ≥ 5 := by numbers
+
+
+-- Example 1.4.6
+-- Exercise: type out the whole proof printed in the text as a Lean proof.
+example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
+  calc
+    n ^ 2
+      = n * n := by ring
+    _ ≥ 5 * n := by rel [hn]
+    _ = 2 * n + 3 * n := by ring
+    _ ≥ 2 * n + 3 * 5 := by rel [hn]
+    _ = 2 * n + 11 + 4 := by ring
+    _ > 2 * n + 11 := by extra
+
+/-
+This one is here just because as the first example using the tactic "extra"
+What does the tactic "extra" do in the following example?
+
+MY ANSWER: Using "extra" tells Lean that there are extra quantities in the inequality that don't
+need to be calculated, but rather support that the value next to the inequality is
+less than/greater than/or equal to (in this example, greater than or equal to) the target quantity
+of the problem.
+-/
+
+-- Example 1.4.7
+example {m n : ℤ} (h : m ^ 2 + n ≤ 2) : n ≤ 2 :=
+  calc
+    n ≤ m ^ 2 + n := by extra
+    _ ≤ 2 := by rel [h]
+
+
+-- Example 1.4.8
+-- Exercise: replace the words "sorry" with the correct Lean justification.
+example {x y : ℝ} (h : x ^ 2 + y ^ 2 ≤ 1) : (x + y) ^ 2 < 3 :=
+  calc
+    (x + y) ^ 2 ≤ (x + y) ^ 2 + (x - y) ^ 2 := by extra
+    _ = 2 * (x ^ 2 + y ^ 2) := by ring
+    _ ≤ 2 * 1 := by rel [h]
+    _ < 3 := by numbers
+
+
+-- Example 1.4.9
+-- Exercise: replace the words "sorry" with the correct Lean justification.
+example {a b : ℚ} (h1 : a ≥ 0) (h2 : b ≥ 0) (h3 : a + b ≤ 8) :
+    3 * a * b + a ≤ 7 * b + 72 :=
+  calc
+    3 * a * b + a
+      ≤ 2 * b ^ 2 + a ^ 2 + (3 * a * b + a) := by extra
+    _ = 2 * ((a + b) * b) + (a + b) * a + a := by ring
+    _ ≤ 2 * (8 * b) + 8 * a + a := by rel [h3]
+    _ = 7 * b + 9 * (a + b) := by ring
+    _ ≤ 7 * b + 9 * 8 := by rel [h3]
+    _ = 7 * b + 72 := by ring
+
+
+/-
+This example is here because Heather Macbeth said it's "wild".
+Why does she say it's wild other than because there's so much algebra?
+
+MY ANSWER: It's "wild" because it invokes the nonegativity of squares for multiple separate squares.
+-/
+
+-- Example 1.4.10
+example {a b c : ℝ} :
+    a ^ 2 * (a ^ 6 + 8 * b ^ 3 * c ^ 3) ≤ (a ^ 4 + b ^ 4 + c ^ 4) ^ 2 :=
+  calc
+    a ^ 2 * (a ^ 6 + 8 * b ^ 3 * c ^ 3)
+      ≤ 2 * (a ^ 2 * (b ^ 2 - c ^ 2)) ^ 2 + (b ^ 4 - c ^ 4) ^ 2
+          + 4 * (a ^ 2 * b * c - b ^ 2 * c ^ 2) ^ 2
+          + a ^ 2 * (a ^ 6 + 8 * b ^ 3 * c ^ 3) := by extra
+    _ = (a ^ 4 + b ^ 4 + c ^ 4) ^ 2 := by ring
+
+
+/-! # Exercises
+
+Solve these problems yourself.  You may find it helpful to solve them on paper before typing them
+up in Lean. -/
+
+/- Exercise #1 -/
+example {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 :=
+  calc
+    a + b
+      = 1/2 * a + 1/2 * (a + 2 * b) := by ring
+    _ ≥ 1/2 * a + 1/2 * 4 := by rel [h2]
+    _ = 1/2 * a + 2 := by ring
+    _ ≥ 1/2 * 3 + 2 := by rel [h1]
+    _ ≥ 3 := by numbers
+
+
+/- Exercise #2 -/
+example {n : ℤ} (hn : n ≥ 10) : n ^ 4 - 2 * n ^ 2 > 3 * n ^ 3 :=
+  calc
+    n ^ 4 - 2 * n ^ 2
+      = n ^ 4 - 3 * n ^ 3 - 2 * n ^ 2 + 3 * n ^ 3 := by ring
+    _ = n ^ 2 * (n ^ 2 - 3 * n - 2) + 3 * n ^ 3 := by ring
+    _ = n ^ 2 * ((n + 7) * (n - 10) + 68)  + 3 * n ^ 3 := by ring
+    _ ≥ 10 ^ 2 * ((10 + 7) * (10 - 10) + 68) + 3 * n ^ 3 := by rel [hn]
+    _ = 100 * (17 * 0 + 68) + 3 * n ^ 3 := by ring
+    _ > 3 * n ^ 3 := by extra
+
+
+/- Exercise #3 -/
+example {n : ℤ} (h1 : n ≥ 5) : n ^ 2 - 2 * n + 3 > 14 :=
+  calc
+    n ^ 2 - 2 * n + 3
+      = n * n - n - n + 3 := by ring
+    _ ≥ 5 * n - n - n + 3 := by rel [h1]
+    _ = 3 * n + 3 := by ring
+    _ ≥ 3 * 5 + 3 := by rel [h1]
+    _ ≥ 18 := by numbers
+    _ > 14 := by numbers
+
+
+/- Exercise #4 -/
+example (a b : ℝ) : a ^ 2 + b ^ 2 ≥ 2 * a * b :=
+  calc
+    a ^ 2 + b ^ 2
+      = a ^ 2 + b ^ 2 - 2 * a * b + 2 * a * b:= by ring
+    _ = (a - b) ^ 2 + 2 * a * b := by ring
+    _ ≥ 2 * a * b := by extra
